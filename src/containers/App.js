6c1b9f6ae9b1a792f9ae57c52b66dc1004f3d1a3
Takaras <ges.dim@gmail.com>
2019-03-30 19:38:12 +0100

    Added error handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 class App extends Component {
@@ -33,7 +34,9 @@ class App extends Component {
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filterRobots} />
+          <ErrorBoundry>
+            <CardList robots={filterRobots} />
+          </ErrorBoundry>
         </div>
       );
     }
