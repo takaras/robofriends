29e916e5c3eff314f2c0a05ca1d545446fac5cc0
Takaras <ges.dim@gmail.com>
2020-04-08 22:13:08 +0200

    Get robots with redux and redux-thunk middleware

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,46 +4,47 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    searchField: state.searchField,
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
   };
 };
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      // searchfield: '',
-    };
-  }
-
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
   }
   // onSearchChange = event => {
   //   this.setState({ searchfield: event.target.value });
   // };
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
-    const filterRobots = robots.filter(robot => {
+    const {
+      searchField,
+      onSearchChange,
+      robots,
+      isPending,
+      error,
+    } = this.props;
+    const filterRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    if (!robots.length) {
+    if (isPending) {
       return <h1>Loading</h1>;
+    } else if (error) {
+      return <h2 className="heading-2">Couldn't find any robots</h2>;
     } else {
       return (
         <div className="tc">
@@ -58,7 +59,4 @@ class App extends Component {
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
